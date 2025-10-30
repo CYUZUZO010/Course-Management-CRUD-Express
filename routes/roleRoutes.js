e0ade092cbde9bebacd385c0dd119e5c34ce11c1
CYUZUZO010 <sandcyuzuzo@gmail.com>
2025-10-30 20:35:46 +0200

    Update roleRoutes.js

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const roleController = require('../controllers/roleController');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
-
+//create and get roles.
 router.post('/',  roleController.createRole);
 router.get('/', roleController.getRoles);
 
