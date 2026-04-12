59048d59048bcc2508257bf25220f7937101e71a
CYUZUZO010 <sandcyuzuzo@gmail.com>
2026-04-12 14:33:16 +0200

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
-
 module.exports = async (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
 
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
   }
-
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
     req.user = decoded;
